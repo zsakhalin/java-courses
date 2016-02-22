303460c2796e1ff8db53e4486d6575d27ffe21b4
zsakhalin <zhilinaaa@gmail.com>
2016-02-22 11:57:06 +0200

    Oprations +

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
 public class Calculate{
-	public static void main(String[] args) {
-		System.out.println("Calculate.");	
+	public static void main(String[] arg) {
+		System.out.println("Calculate.");
+		int a = Integer.valueOf(arg[0]); 
+		int b= Integer.valueOf(arg[1]);
+		int c=a+b;
+		System.out.println("Summ " + c);	
 	}	
 }
